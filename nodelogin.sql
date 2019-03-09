ffaa69279842236b2125d5524f185e5aa8e9c3df
Galuh Aan Ramadhan <aan.ramadhan77@gmail.com>
2019-03-09 16:21:37 +0700

    db fix connect

--- nodelogin.sql
+++ nodelogin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 03, 2019 at 05:12 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 09, 2019 at 10:19 AM
+-- Server version: 8.0.15
+-- PHP Version: 7.2.15-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,18 +31,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES
-(1, 'test', 'test', 'test@test.com'),
-(2, 'vicky', '12321', 'vicky@gmail.com'),
-(3, 'aan', 'aan', 'aan@gmail.com');
+INSERT INTO `accounts` (`id`, `username`, `password`) VALUES
+(1, 'avcsvcvcv', 'adrefeadfdf'),
+(2, 'aan', 'aan'),
+(3, 'abid', 'abid'),
+(4, 'vicky', '123');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,7 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
